8861b0d3a31bb365f40991c5a4bd74598cc52c49
cavalcantenogueira-collab <cavalcante.nogueira@escola.pr.gov.br>
2026-05-29 17:49:20 -0300

    CODEE

--- script .js
+++ script .js
@@ -2,79 +2,53 @@ const botao = document.getElementById("saibaMais");
 const mensagem = document.getElementById("mensagem");
 
 botao.addEventListener("click", () => {
-
     mensagem.textContent =
-    "A sustentabilidade no campo garante produtividade hoje e recursos para as futuras gerações.";
-
+    "A sustentabilidade garante produção agrícola e preservação ambiental.";
 });
 
 const darkBtn = document.getElementById("darkModeBtn");
 
 darkBtn.addEventListener("click", () => {
-
     document.body.classList.toggle("dark");
-
 });
 
 const formulario = document.getElementById("formulario");
 
-formulario.addEventListener("submit", function(event){
+formulario.addEventListener("submit", function(e){
 
-    event.preventDefault();
+    e.preventDefault();
 
     const nome = document.getElementById("nome").value;
     const email = document.getElementById("email").value;
-
     const resultado = document.getElementById("resultado");
 
     if(nome === "" || email === ""){
-
-        resultado.textContent =
-        "Preencha todos os campos.";
-
+        resultado.textContent = "Preencha todos os campos.";
         resultado.style.color = "red";
-
-    }
-    else{
-
-        resultado.textContent =
-        "Cadastro realizado com sucesso!";
-
+    } else {
+        resultado.textContent = "Cadastro realizado com sucesso!";
         resultado.style.color = "green";
-
     }
-
 });
 
 const sections = document.querySelectorAll("section");
-const navLinks = document.querySelectorAll(".menu-link");
+const links = document.querySelectorAll(".menu-link");
 
 window.addEventListener("scroll", () => {
 
     let current = "";
 
-    sections.forEach(section => {
-
-        const sectionTop = section.offsetTop - 200;
-
-        if(window.scrollY >= sectionTop){
-
-            current = section.getAttribute("id");
-
+    sections.forEach(sec => {
+        if(window.scrollY >= sec.offsetTop - 200){
+            current = sec.getAttribute("id");
         }
-
     });
 
-    navLinks.forEach(link => {
-
+    links.forEach(link => {
         link.classList.remove("active");
-
         if(link.getAttribute("href") === "#" + current){
-
             link.classList.add("active");
-
         }
-
     });
 
 });
